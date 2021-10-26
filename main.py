435dee02e043080200fc1810c5c4096cf99d1b7e
kalleoOuriques <90047255+kalleoOuriques@users.noreply.github.com>
2021-10-26 19:24:34 -0300

    Add files via upload
    Começo do projeto, menus de seleção tipo e modo. Estágio de arrumar a resposta no terminal perante ao input de entrada.

---
+++ main.py
@@ -0,0 +1,193 @@
+import random
+while True:
+    sentimentos = ['alegria', 'amor', 'cidadania', 'esperança','felicidade', 'harmonia',  'paz', 'perseverança', 'saudades','solidariedade',  'tristeza', 'odio', 'raiva']
+
+    frutas = ['abacate', 'abacaxi', 'acerola', 'banana', 'caju''igo','framboesa', 'goiaba', 'kiwi', 'laranja', 'limão', 'maçã', 'mamão', 'melão', 'pêssego', 'tangerina', 'manga']
+
+    times = ['atlético', 'botafogo', 'corinthians', 'coritiba', 'cruzeiro', 'flamengo','fluminense', 'goiás','grêmio','internacional', 'náutico', 'palmeiras',  'santos',  'vasco', 'avaí', 'figueirense']
+
+    menu_1 = input('TIPO DE JOGO \n\n[1]SENTIMENTOS\n[2]FRUTAS\n[3]TIMES\n\nDigite o número do tipo escolhido: ')
+
+    while menu_1 not in ('1','2','3'):
+        menu_1 = input('TIPO DE JOGO \n\n[1]SENTIMENTOS\n[2]FRUTAS\n[3]TIMES\n\nDigite o número do tipo escolhido: ')
+
+    if menu_1 == '1':
+        escolha_1 = sentimentos[:]
+    elif menu_1 == '2':
+        escolha_1 = frutas[:]
+    elif menu_1 == '3':
+        escolha_1 = times[:]
+
+    index = random.randint(0,len(escolha_1))
+    menu_2 = input('DIFICULDADE DO JOGO \n\n[1]FÁCIL(9 CHANCES)\n[2]MÉDIO(6 CHANCES)\n[3]DIFÍCIL(3 CHANCES)\n\nDigite o número do tipo escolhido: ')
+    while menu_2 not in ('1','2','3'):
+        menu_2 = input('DIFICULDADE DO JOGO \n\n[1]FÁCIL(9 CHANCES)\n[2]MÉDIO(6 CHANCES)\n[3]DIFÍCIL(3 CHANCES)\n\nDigite o número do tipo escolhido: ')
+
+    if menu_2 == '3':
+            print("  _______     ")
+            print(" |/      |    ")
+            print(" |            ")
+            print(" |            ")
+            print(" |            ")
+            print(" |            ")
+            resposta = '  ', '___  ' * index
+            print(resposta)
+            entrada = input('Digite uma letra: ')
+
+    if entrada in (escolha_1[index]):
+
+                print("  _______     ")
+                print(" |/      |    ")
+                print(" |            ")
+                print(" |            ")
+                print(" |            ")
+                print(" |            ")
+                resposta = '   ' + '___  ' * (index-1) + '  ' + escolha_1[index]
+                print(resposta)
+    else:
+                erro
+                print("  _______     ")
+                print(" |/      |    ")
+                print(" |      (_)   ")
+                print(" |            ")
+                print(" |            ")
+                print(" |            ")
+                print(resposta)
+            if :
+                print("  _______     ")
+                print(" |/      |    ")
+                print(" |      (_)   ")
+                print(" |      \|/   ")
+                print(" |            ")
+                print(" |            ")
+
+            if :
+                    print("  _______     ")
+                    print(" |/      |    ")
+                    print(" |      (_)   ")
+                    print(" |      \|/   ")
+                    print(" |       |    ")
+                    print(" |      / \   ")
+            escolha_2 = 9
+
+    elif menu_2 == '2':
+        if:
+            print("  _______     ")
+            print(" |/      |    ")
+            print(" |      (_    ")
+            print(" |            ")
+            print(" |            ")
+            print(" |            ")
+            if:
+                print("  _______     ")
+                print(" |/      |    ")
+                print(" |      (_)   ")
+                print(" |       |    ")
+                print(" |            ")
+                print(" |            ")
+                if:
+                    print("  _______     ")
+                    print(" |/      |    ")
+                    print(" |      (_)   ")
+                    print(" |      \|/   ")
+                    print(" |            ")
+                    print(" |            ")
+                    if:
+                        print("  _______     ")
+                        print(" |/      |    ")
+                        print(" |      (_)   ")
+                        print(" |      \|/   ")
+                        print(" |       |    ")
+                        print(" |            ")
+                        if:
+                            print("  _______     ")
+                            print(" |/      |    ")
+                            print(" |      (_)   ")
+                            print(" |      \|/   ")
+                            print(" |       |    ")
+                            print(" |        \   ")
+                            if:
+                                print("  _______     ")
+                                print(" |/      |    ")
+                                print(" |      (_)   ")
+                                print(" |      \|/   ")
+                                print(" |       |    ")
+                                print(" |      / \   ")
+                                if:
+                                    if:
+                                        print("  _______     ")
+                                        print(" |/      |    ")
+                                        print(" |      (_)   ")
+                                        print(" |      \|/   ")
+                                        print(" |       |    ")
+                                        print(" |      / \   ")
+
+        escolha_2 = 6
+    elif menu_2 == '1':
+        print("  _______     ")
+        print(" |/      |    ")
+        print(" |      (     ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+        if erros == 2:
+            print("  _______     ")
+            print(" |/      |    ")
+            print(" |      (_    ")
+            print(" |            ")
+            print(" |            ")
+            print(" |            ")
+            if erros == 3 :
+                print("  _______     ")
+                print(" |/      |    ")
+                print(" |      (_)   ")
+                print(" |            ")
+                print(" |            ")
+                print(" |            ")
+                if erros == 4:
+                    print("  _______     ")
+                    print(" |/      |    ")
+                    print(" |      (_)   ")
+                    print(" |       |    ")
+                    print(" |            ")
+                    print(" |            ")
+                    if erros == 5:
+                        print("  _______     ")
+                        print(" |/      |    ")
+                        print(" |      (_)   ")
+                        print(" |      \|    ")
+                        print(" |            ")
+                        print(" |            ")
+                        if erros == 6:
+                            print("  _______     ")
+                            print(" |/      |    ")
+                            print(" |      (_)   ")
+                            print(" |      \|/   ")
+                            print(" |            ")
+                            print(" |            ")
+                            if erros == 7:
+                                print("  _______     ")
+                                print(" |/      |    ")
+                                print(" |      (_)   ")
+                                print(" |      \|/   ")
+                                print(" |       |    ")
+                                print(" |            ")
+                                if erros == 8:
+                                    print("  _______     ")
+                                    print(" |/      |    ")
+                                    print(" |      (_)   ")
+                                    print(" |      \|/   ")
+                                    print(" |       |    ")
+                                    print(" |      /     ")
+                                    if erros == 9:
+                                        print("  _______     ")
+                                        print(" |/      |    ")
+                                        print(" |      (_)   ")
+                                        print(" |      \|/   ")
+                                        print(" |       |    ")
+                                        print(" |      / \   ")
+
+
+        escolha_2 = 3
+
+
